e4cbeb6fb072b9f0a6d96488668bb00484241008
Lars Bärring <lars.barring@smhi.se>
2024-11-26 14:52:00 +0100

    Rename xslt file and changed README.md formatting

--- transform.sh
+++ transform.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for x in base derived accepted; do
-    xsltproc udunits2-general.xslt  udunits2-$x.xml > udunits2-$x.html
+    xsltproc xml2html.xslt  udunits2-$x.xml > udunits2-$x.html
 done
